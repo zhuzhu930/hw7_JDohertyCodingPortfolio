0ff36f86a97885cf40c08dd7a975f42cd1dfba9d
Jessie Doherty <zhuzhu930@yahoo.com>
2022-04-29 00:30:50 -0500

    initial js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
 //connecting the DOM
-//
+const navBar = document.getElementById("navBar");
+const navBtn = document.getElementById("navBtn");
+
+// navBtn.addEventListener("click")
+// const btnHTML = `<div class="mb-3">
+// <label for="exampleFormControlInput1" class="form-label">Email</label>
+// <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+// </div>
+// <div class="mb-3">
+// <label for="exampleFormControlTextarea1" class="form-label">Your message</label>
+// <textarea class="form-control" id="messageInput" rows="3"></textarea>
+// </div>`;
